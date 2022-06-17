bb83b7ff17114d95e7aa77f20ad14d2bff40e957
el-javo <javimuu99@gmail.com>
2022-06-17 11:43:58 +0200

    All animations look good and in shape

--- public/app.js
+++ public/app.js
@@ -1,6 +1,7 @@
 const modusImage = document.querySelector("#modus_img")
 const textgen = document.querySelector("#textgen")
 const checkthis = document.querySelector('#checkthis')
+const check_cont = document.querySelector('.check_cont')
 
 function wait(milliseconds) {
     const date = Date.now();
@@ -9,14 +10,20 @@ function wait(milliseconds) {
       currentDate = Date.now();
     } while (currentDate - date < milliseconds);
 }
-const showText = ()=>{
-    textgen.classList.toggle('isActive')
-}
 
 const generateText = ()=>{
     modusImage.classList.toggle('isActive')
+    setTimeout(function() {
+        modusImage.style.display = "none"
+    },400)
     checkthis.classList.toggle('isActive')
-    showText()
+    setTimeout(function() {
+        check_cont.style.display = "flex"
+    },2000)
+    setTimeout(function() {
+        check_cont.style.display = "none"
+    },5000)
+    textgen.classList.toggle('isActive')
 }
 
 modusImage.addEventListener('click', generateText)
