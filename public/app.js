97ec775328c936fabd035db4d34cdfd7864f8cdb
el-javo <javimuu99@gmail.com>
2022-06-27 13:39:52 +0200

    added more boxes to the generation panel and updated server to not ask for verification to do a no up generation

--- public/app.js
+++ public/app.js
@@ -1,3 +1,4 @@
+
 const modusImage = document.querySelector("#modus_img")
 const textgen = document.querySelector("#textgen")
 const checkthis = document.querySelector('#checkthis')
@@ -5,7 +6,7 @@ const check_cont = document.querySelector('.check_cont')
 const navbar = document.querySelector('.navbar')
 const navbarlogo = document.querySelector('.navbar_logo')
 const ratiobar = document.querySelector('.ratio')
-const ratioVal = document.querySelector('.ratioval')
+const ratioVal = document.querySelector('#ratioval')
 const navbar_burger = document.querySelector('.navbar_burger')
 const navbar_menu = document.querySelector('.navbar_menu')
 const jpg_selector = document.querySelector('#jpg')
@@ -14,7 +15,12 @@ const borde_selektor = document.querySelector('#borde_selektor')
 const generate_txt = document.querySelector('#generate_txt')
 const generate_cont = document.querySelector('#generate_cont')
 const loading_txt = document.querySelector('#loading_txt')
-
+const file = document.querySelector('#upload_img')
+const form_cont_img = document.querySelector('#form_cont_img')
+const invert_cont = document.querySelector('#invert_cont')
+const invert_txt = document.querySelector('#invert_txt')
+const contrastbar = document.querySelector('#contrast')
+const contrastVal = document.querySelector('#contrastval')
 
 
 function wait(milliseconds) {
@@ -73,19 +79,75 @@ const format_selector_toggle = (wich)=>{
     }
 }
 
-
 const generate_animation_toggle =()=>{
     generate_txt.classList.toggle('loading')
     loading_txt.classList.toggle('loading')
 }
 
+const generateImgAPI = async  ()=>{
+    const url = borde_selektor.classList.contains('isJpg') ? "https://image2ascii.up.railway.app/api/v1/gen/generateImgNoUp":"https://image2ascii.up.railway.app/api/v1/gen/generateTxtNoUp"
+    const image = file.files[0]
+    const inverted = invert_cont.classList.contains('inverted') ? 'true':'false'
+    const ratio = ratiobar.value
+    const contrast = contrastbar.value
+
+    if(!image){ 
+        form_cont_img.classList.add('badData')
+        console.log('hi');
+        return
+    }
+    
+    const formData = new FormData();
+    formData.append('image', image)
+    formData.append('invert', inverted)
+    formData.append('contrast', contrast)
+    formData.append('resolution', ratio)
+    if(borde_selektor.classList.contains('isJpg')){
+        const bgColor = invert_cont.classList.contains('inverted') ? '0':'255'
+        const fontColor = invert_cont.classList.contains('inverted') ? '255':'0'
+        formData.append('backgroundColor', bgColor)
+        formData.append('backgroundColor', bgColor)
+        formData.append('backgroundColor', bgColor)
+        formData.append('fontColor', fontColor)
+        formData.append('fontColor', fontColor)
+        formData.append('fontColor', fontColor)
+    }
+
+    // const res = await axios({
+    //     method:'post',
+    //     url:url,
+    //     data:formData,
+    //     headers:{
+    //         'Accept':'*/*',
+    //         'Accept-Encoding': 'gzip, deflate, br',
+    //         'Connection': 'keep-alive'
+    //     }
+    // })
+    console.log('send')
+    form_cont_img.classList.remove('badData')
+    generate_animation_toggle()
+    const res = await axios.post(url, formData)
+    console.log('done')
+}
+const generate = ()=>{
+    generateImgAPI()
+}
+
+const inverted = ()=>{
+    invert_cont.classList.toggle('inverted')
+    invert_txt.classList.toggle('inverted')
+}
 
 modusImage.addEventListener('click', generateText)
 window.addEventListener('scroll', activeNavbar_scroll)
 navbar_burger.addEventListener('click', toggle_burger_deploy)
 jpg_selector.addEventListener('click',format_selector_toggle('jpg'))
 txt_selector.addEventListener('click',format_selector_toggle('txt'))
-generate_cont.addEventListener('click',generate_animation_toggle)
+generate_cont.addEventListener('click',generate)
+invert_cont.addEventListener('click', inverted)
 ratiobar.addEventListener("input", function(){
     ratioVal.innerHTML = ratiobar.value
+})
+contrastbar.addEventListener("input",function(){
+    contrastVal.innerHTML = contrastbar.value
 })
--- routes/generateRoutes.js
+++ routes/generateRoutes.js
@@ -5,8 +5,8 @@ const Router = express.Router()
 
 Router.post('/generateImg',[authenticateUser,buildReqBody({format:'img',upload:true})], generate)
 Router.post('/generateTxt', [authenticateUser,buildReqBody({format:'txt',upload:true})], generate)
-Router.post('/generateImgNoUp',[authenticateUser,buildReqBody({format:'img',upload:false})], generate)
-Router.post('/generateTxtNoUp', [authenticateUser,buildReqBody({format:'txt',upload:false})], generate)
+Router.post('/generateImgNoUp',buildReqBody({format:'img',upload:false}), generate)
+Router.post('/generateTxtNoUp', buildReqBody({format:'txt',upload:false}), generate)
 // posibilidad de añadir unas de tipo get tipo publica que generen archivos temporales para su facil uso
 
 module.exports = Router
